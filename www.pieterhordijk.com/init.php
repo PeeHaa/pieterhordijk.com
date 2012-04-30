fa7ef6e18f7a7afd6cea2590f6c1a4b4b61a7a56
PeeHaa <info@pieterhordijk.com>
2012-05-01 01:11:50 +0200

    Implemented database connection and modelfactory

--- www.pieterhordijk.com/init.php
+++ www.pieterhordijk.com/init.php
@@ -25,9 +25,25 @@ $request = new MFW_Http_Request($_SERVER['SCRIPT_URI']);
  $router = new MFW_Router_Rewrite($routes, $request);
 
 /**
+ * Get the database connection
+ */
+$databaseConnection = new MFW_Db_Connection(MFW_DB_ENGINE, MFW_DB_NAME, MFW_DB_HOST, MFW_DB_PORT, MFW_DB_USERNAME, MFW_DB_PASSWORD);
+
+/**
+ * Get the model factory
+ */
+$modelFactory = new ModelFactory($databaseConnection);
+
+/**
  * Get the view
  */
-$view = new MFW_View($router, MFW_SITE_PATH.'/code/views');
+$view = new MFW_View($router, MFW_SITE_PATH.'/code/views', $modelFactory);
+
+/**
+ * Instantiate the user
+ */
+$user = new MFW_User();
+$view->user = $user->getCurrentUser();
 
 /**
  * Create an instance of the front controller
