78feb66745f6cb8a240d906bb199dd7ed159c3d1
PeeHaa <info@pieterhordijk.com>
2012-05-05 01:08:22 +0200

    Added contact form Implemented logo upload Changed submit text

--- www.pieterhordijk.com/code/models/Forms.php
+++ www.pieterhordijk.com/code/models/Forms.php
@@ -10,10 +10,15 @@ class Project_Create_Form extends MFW_Form
         $this->addField('slug', new MFW_Form_Field_Text(array('required'=>true)));
         $this->addField('intro', new MFW_Form_Field_Textarea(array('required'=>false)));
         $this->addField('description', new MFW_Form_Field_Textarea(array('required'=>false)));
+        $this->addField('image', new MFW_Form_Field_File(array('required'=>false,
+                                                               'max_size'=>20971520,
+                                                               'save_path'=>MFW_SITE_PATH.'/public/img/projects',
+                                                               'save_name'=>'hash',
+                                                               'allowed_extensions' => array('png', 'jpg', 'jpeg', 'gif'))));
         $this->addField('github', new MFW_Form_Field_Text(array('required'=>false)));
         $this->addField('download', new MFW_Form_Field_Text(array('required'=>false)));
         $this->addField('version', new MFW_Form_Field_Text(array('required'=>false)));
-        $this->addField('submit', new MFW_Form_Field_Submit(array('initial'=>'Create')));
+        $this->addField('submit', new MFW_Form_Field_Submit(array('initial'=>'Save new project')));
     }
 }
 
@@ -41,5 +46,19 @@ class User_Login_Form extends MFW_Form
         $this->addField('password', new MFW_Form_Field_Password(array('required'=>true,
                                                                       'attributes'=>array('placeholder'=>'Password'))));
         $this->addField('submit', new MFW_Form_Field_Submit(array('initial'=>'')));
+    }
+}
+
+class Contact_Form extends MFW_Form
+{
+    function __construct()
+    {
+        parent::__construct(True);
+
+        $this->addField('name', new MFW_Form_Field_Text(array('required'=>true)));
+        $this->addField('email', new MFW_Form_Field_Email(array('required'=>true)));
+        $this->addField('subject', new MFW_Form_Field_Text(array('required'=>true)));
+        $this->addField('message', new MFW_Form_Field_Textarea(array('required'=>true)));
+        $this->addField('submit', new MFW_Form_Field_Submit(array('initial'=>'Send')));
     }
 }
