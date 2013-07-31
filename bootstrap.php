3cfb6f50473220597e16b5022a415a7f41399ab6
PeeHaa <info@pieterhordijk.com>
2013-07-31 17:28:41 +0200

    Moved environment specific stuff

--- bootstrap.php
+++ bootstrap.php
@@ -18,37 +18,9 @@ use PieterHordijk\Core\Autoloader,
     PieterHordijk\Storage\Session as SessionStorage;
 
 /**
- * Setup error reporting
- */
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('log_errors', 0);
-
-/**
- * Setup timezone
- */
-ini_set('date.timezone', 'Europe/Amsterdam');
-
-/**
  * Bootstrap the PitchBlade library
  */
-//require_once __DIR__ . '/src/WebMonitor/bootstrap.php';
-
-/**
- * Setup the database connection
- */
-/*
-$dbConnection = new \PDO(
-    'pgsql:dbname=pieterhordijk;host=127.0.0.1',
-    'user',
-    'pass',
-    [
-        \PDO::ATTR_EMULATE_PREPARES   => false,
-        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    ]
-);
-*/
+require_once __DIR__ . '/src/PieterHordijk/bootstrap.php';
 
 /**
  * Setup session
