4c7685b940271cb0948bf0135e009ba8f2bfa4f9
PeeHaa <info@pieterhordijk.com>
2013-07-31 16:51:44 +0200

    Moved html to templates

---
+++ bootstrap.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * This bootstraps the application
+ *
+ * PHP version 5.4
+ *
+ * @category   PieterHordijk
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PieterHordijk;
+
+use PieterHordijk\Core\Autoloader,
+    PieterHordijk\Http\Session,
+    PieterHordijk\Http\Request,
+    PieterHordijk\Storage\Session as SessionStorage;
+
+/**
+ * Setup error reporting
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 0);
+
+/**
+ * Setup timezone
+ */
+ini_set('date.timezone', 'Europe/Amsterdam');
+
+/**
+ * Bootstrap the PitchBlade library
+ */
+//require_once __DIR__ . '/src/WebMonitor/bootstrap.php';
+
+/**
+ * Setup the database connection
+ */
+/*
+$dbConnection = new \PDO(
+    'pgsql:dbname=pieterhordijk;host=127.0.0.1',
+    'user',
+    'pass',
+    [
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    ]
+);
+*/
+
+/**
+ * Setup session
+ */
+/*
+$session = new Session($dbConnection);
+session_set_save_handler($session, true);
+session_start();
+*/
+
+/**
+ * Setup autoloader for the application
+ */
+/*
+$autoloader = new Autoloader(__NAMESPACE__, dirname(__DIR__));
+$autoloader->register();
+*/
+
+/**
+ * Setup the request object
+ */
+//$request = new Request($_SERVER, $_GET, $_POST, $_COOKIE);
+
+/**
+ * Setup the session object
+ */
+//$session = new SessionStorage();
+
+/**
+ * Get the template
+ */
+/*
+if ($request->getMethod() == 'GET' && $request->getPath() == '/about') {
+    $template = __DIR__ . '/template/about.phtml';
+} elseif ($request->getMethod() == 'GET' && $request->getPath() == '/graph') {
+    $template = __DIR__ . '/template/graph.pjson';
+} elseif ($request->getMethod() == 'GET' && $request->getPath() == '/add-monitor') {
+    $template = __DIR__ . '/template/add-monitor.phtml';
+} elseif ($request->getMethod() == 'POST' && $request->getPath() == '/add-monitor') {
+    $template = __DIR__ . '/template/add-monitor.phtml';
+} elseif ($request->getMethod() == 'GET' && $request->getPath() == '/logout') {
+    $template = __DIR__ . '/template/logout.phtml';
+} elseif ($request->getMethod() == 'GET' && $request->getPath() == '/login') {
+    $template = __DIR__ . '/template/login.phtml';
+} elseif ($request->getMethod() == 'POST' && $request->getPath() == '/login') {
+    $template = __DIR__ . '/template/login.phtml';
+} elseif ($request->getMethod() == 'GET' && $request->getPath() == '/register') {
+    $template = __DIR__ . '/template/register.phtml';
+} elseif ($request->getMethod() == 'POST' && $request->getPath() == '/register') {
+    $template = __DIR__ . '/template/register.phtml';
+} elseif ($request->getMethod() == 'GET' && $request->getPath() == '/request-invitation') {
+    $template = __DIR__ . '/template/invitation-requested.phtml';
+} elseif ($request->getMethod() == 'POST' && $request->getPath() == '/request-invitation') {
+    $template = __DIR__ . '/template/register.phtml';
+} else {
+    $template = __DIR__ . '/template/main.phtml';
+}
+*/
+
+/**
+ * Render the page
+ */
+/*
+ob_start();
+require $template;
+$content = ob_get_contents();
+ob_end_clean();
+*/
+
+require __DIR__ . '/templates/page.phtml';
