2677ebf71d9bbb0cc6903e46bc68a112d6bf3933
PeeHaa <info@pieterhordijk.com>
2013-07-31 16:31:47 +0200

    Added dummy content and footer

--- public/index.php
+++ public/index.php
@@ -21,6 +21,7 @@
                         <li><a href="#">Home</a></li>
                         <li><a href="#">Open source</a></li>
                         <li><a href="#">Projects</a></li>
+                        <li><a href="#">Demos</a></li>
                         <li><a href="#">Contact</a></li>
                         <li><a href="#">About</a></li>
                     </ul>
@@ -28,8 +29,50 @@
             </div>
         </header>
         <main>
+            <h1>PieterHordijk.com</h1>
+            <p>This site mainly exists as an index for the projects I'm working on and because people have been bugging me about setting up my site again for more then a year.</p>
+            <p>On this site you will find all the projects I have worked on or am still working on both open source and closed source.</p>
+            <h2>Can I steal this site?</h2>
+            <p>I have no idea why you would want to steal my site (I'm a developer and not a designer), but <a href="#">here you go</a>. It is <a href="http://www.dbad-license.org/" target="_blank">DBAD</a> licensed.</p>
         </main>
         <footer>
+            <div class="content">
+                <ul>
+                    <li><h1>Open source</h1>
+                        <ul>
+                            <li><a href="#">OpCacheGUI - a nice webinterface for PHP's OpCache</a></li>
+                            <li><a href="#">HexDump - an online hex viewer</a></li>
+                            <li><a href="#">RichUploader - a private filehoster</a></li>
+                            <li><a href="#">PHP OAuth library</a></li>
+                            <li><a href="#">Proposal for the new beginners tutorial on php.net</a></li>
+                            <li><a href="#">Show all</a></li>
+                        </ul>
+                    </li>
+                    <li><h1>Demos</h1>
+                        <ul>
+                            <li><a href="#">HexDump - an online hex viewer</a></li>
+                            <li><a href="#">RichUploader - a private filehoster</a></li>
+                            <li><a href="#">WebMonitor - Monitors you servers for you</a></li>
+                            <li>&nbsp;</li>
+                            <li>&nbsp;</li>
+                            <li><a href="#">Show all</a></li>
+                        </ul>
+                    </li>
+                    <li class="flair">
+                        <ul>
+                            <li>
+                                <a href="http://stackoverflow.com/users/508666/peehaa">
+                                    <img src="http://stackoverflow.com/users/flair/508666.png" width="208" height="58" alt="profile for PeeHaa at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for PeeHaa at Stack Overflow, Q&amp;A for professional and enthusiast programmers">
+                                </a>
+                            </li>
+                            <li><iframe src="http://githubbadge.appspot.com/badge/PeeHaa?a=0" style="border: 0;height: 142px;width: 200px;overflow: hidden;" frameBorder="0"></iframe></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <ul class="legal">
+                <li>Copyright © 2013 Pieter Hordijk</li>
+            </ul>
         </footer>
         <script src="/js/main.js"></script>
     </body>
