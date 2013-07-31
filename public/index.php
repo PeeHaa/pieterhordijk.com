f6c13100f2dc8472f8f01c9e91689fd45e3cf6c3
PeeHaa <info@pieterhordijk.com>
2013-07-31 15:19:14 +0200

    Added header

--- public/index.php
+++ public/index.php
@@ -1,3 +1,36 @@
 <?php
 
-require __DIR__ . '/../bootstrap.php';
+//require __DIR__ . '/../bootstrap.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pieter Hordijk</title>
+        <meta charset="utf-8">
+        <script>'article aside footer header main nav section time'.replace(/\w+/g,function(n){document.createElement(n)})</script>
+        <link href='//fonts.googleapis.com/css?family=Lato:300italic,700italic,300,700' rel='stylesheet' type='text/css'>
+        <link href="/style/style.css" rel="stylesheet" type="text/css" media="screen"></style>
+    </head>
+    <body>
+        <header>
+            <div class="content">
+                <h1>Pieter Hordijk</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Open source</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
+        </main>
+        <footer>
+        </footer>
+        <script src="/js/main.js"></script>
+    </body>
+</html>
